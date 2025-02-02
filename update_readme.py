fe1a3eeb61144a5d3a37e59d1771eec15c9579f8
YenaLey <yena.e121@gmail.com>
2025-02-02 17:16:27 +0900

    Update

--- update_readme.py
+++ update_readme.py
@@ -1,14 +1,12 @@
 import requests
 import datetime
 import os
-from bs4 import BeautifulSoup
 
 USERNAME = "lyn010913"  # 백준 ID
 README_PATH = "README.md"
 TIER_LOG_PATH = "tier_log.txt"  # 티어 변경 로그 저장 파일
-GRASS_IMAGE_PATH = "baekjoon_grass.svg"  # 백준 잔디 그래프 저장 파일
 SOLVED_AC_BADGE = f"https://mazassumnida.wtf/api/v2/generate_badge?boj={USERNAME}"  # Solved.ac 티어 뱃지
-BAEKJOON_PROFILE_URL = f"https://www.acmicpc.net/user/{USERNAME}"  # 백준 프로필 URL
+MAZANDI_BADGE = f"http://mazandi.herokuapp.com/api?handle={USERNAME}&theme=warm"  # Mazandi 백준 잔디 이미지
 
 # 백준 solved.ac API 요청 (현재 티어 가져오기)
 def get_baekjoon_tier(username):
@@ -30,24 +28,6 @@ def convert_tier(tier_num):
         "Ruby V", "Ruby IV", "Ruby III", "Ruby II", "Ruby I"
     ]
     return tiers[tier_num] if 0 <= tier_num < len(tiers) else "Unknown"
-
-# 백준 잔디 그래프 크롤링 & 저장
-def fetch_baekjoon_grass():
-    response = requests.get(BAEKJOON_PROFILE_URL)
-    
-    if response.status_code != 200:
-        print("백준 프로필 페이지를 가져오는 데 실패했습니다.")
-        return
-
-    soup = BeautifulSoup(response.text, "html.parser")
-    svg_element = soup.find("svg", class_="js-calendar-graph-svg")  # 백준 잔디 그래프를 포함하는 SVG 태그 찾기
-    
-    if not svg_element:
-        print("백준 잔디 그래프를 찾을 수 없습니다.")
-        return
-
-    with open(GRASS_IMAGE_PATH, "w", encoding="utf-8") as file:
-        file.write(str(svg_element))
 
 # README 업데이트
 def update_readme():
@@ -86,8 +66,8 @@ def update_readme():
     new_readme = (
         "## 🏆 Baekjoon Tier Badge\n\n"
         f"![Baekjoon Tier]({SOLVED_AC_BADGE})\n\n"
-        "## 🌱 Baekjoon Contribution Graph\n\n"
-        f"<img src=\"./{GRASS_IMAGE_PATH}\" alt=\"Baekjoon Grass\" />\n\n"
+        "## 🌱 Baekjoon Contribution Graph (Mazandi)\n\n"
+        f"![mazandi profile]({MAZANDI_BADGE})\n\n"
         "## 📜 Tier Change Log\n\n"
         "| Date | Tier |\n"
         "|------|------|\n"
@@ -99,5 +79,4 @@ def update_readme():
         file.write(new_readme)
 
 if __name__ == "__main__":
-    fetch_baekjoon_grass()  # 백준 잔디 크롤링
     update_readme()  # README 업데이트
