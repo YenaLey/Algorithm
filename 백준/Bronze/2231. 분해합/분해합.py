90185254b3f9f7d98f733b9557d40b632ce8d7bc
Yena Lee <147997324+YenaLey@users.noreply.github.com>
2025-02-03 22:04:43 +0900

    [Bronze II] Title: 분해합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,10 +1,11 @@
-num1 = input()
+num1 = int(input())
 
+start = max(1, num1 - 9 * len(str(num1)))  # 탐색 범위를 줄이기
 result = 0
-for i in range(1, int(num1)+1):
-    num2 = str(i + sum(map(int, str(i))))
-    if (num1 == num2):
+
+for i in range(start, num1):  # 1부터가 아니라 start부터 시작
+    if i + sum(map(int, str(i))) == num1:
         result = i
         break
 
-print(result) 
+print(result)
