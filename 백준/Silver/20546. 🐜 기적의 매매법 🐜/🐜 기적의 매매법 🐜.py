2c8a7b237b31af48d87c5faf7471ea941ba44a62
Yena Lee <147997324+YenaLey@users.noreply.github.com>
2025-02-11 22:36:03 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
@@ -2,32 +2,38 @@ cash = int(input())
 stock_list = list(map(int, input().split()))
 
 def BNP(cash, stock_list):
-  count = 0
-  for stock in stock_list:
-    if cash >= stock:
-      count += cash // stock
-      cash = cash % stock
-  return cash + stock_list[-1] * count
+    count = 0
+    for stock in stock_list:
+        if cash >= stock:
+            count += cash // stock
+            cash %= stock  # cash = cash % stock (연산 최적화)
+    return cash + stock_list[-1] * count
 
 def TIMING(cash, stock_list):
-  count = 0
+    count = 0
 
-  for i in range(3, len(stock_list)):
+    for i in range(3, len(stock_list)):
+        # 최근 3일간 주가 상승 또는 하락 여부 확인 (가독성 개선)
+        up_trend = stock_list[i] > stock_list[i-1] > stock_list[i-2] > stock_list[i-3]
+        down_trend = stock_list[i] < stock_list[i-1] < stock_list[i-2] < stock_list[i-3]
 
-    if (stock_list[i]-stock_list[i-1] > 0 and stock_list[i-1]-stock_list[i-2] > 0 and stock_list[i-2]-stock_list[i-3] > 0): # 3일 연속 상승한 경우 (전량 매도)
-      cash += count * stock_list[i]
-      count = 0
+        if up_trend:  # 3일 연속 상승 → 전량 매도
+            cash += count * stock_list[i]
+            count = 0
+        elif down_trend:  # 3일 연속 하락 → 전량 매수
+            if cash >= stock_list[i]:
+                count += cash // stock_list[i]
+                cash %= stock_list[i]
 
-    elif (stock_list[i]-stock_list[i-1] < 0 and stock_list[i-1]-stock_list[i-2] < 0 and stock_list[i-2]-stock_list[i-3] < 0): # 3일 연속 하락한 경우 (전량 매수)
-      if cash >= stock_list[i]:
-        count += cash // stock_list[i]
-        cash = cash % stock_list[i]
+    return cash + stock_list[-1] * count
 
-  return cash + stock_list[-1] * count
+# 💡 BNP와 TIMING을 각각 1번만 계산하여 최적화
+bnp_result = BNP(cash, stock_list)
+timing_result = TIMING(cash, stock_list)
 
-if BNP(cash, stock_list) > TIMING(cash, stock_list):
-  print("BNP")
-elif BNP(cash, stock_list) < TIMING(cash, stock_list):
-  print("TIMING")
+if bnp_result > timing_result:
+    print("BNP")
+elif bnp_result < timing_result:
+    print("TIMING")
 else:
-  print("SAMESAME")
+    print("SAMESAME")
